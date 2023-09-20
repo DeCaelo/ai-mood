5a0a7d9cade7e55f8a93002bcd09037b737c9cee
DeCaelo <ludovic.cleuet@gmail.com>
2023-09-20 13:09:20 +0200

    getEntries & getUserFromClerkID

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,4 +1,26 @@
+import { getUserFromClerkID } from '@/utils/auth';
+import { prisma } from '@/utils/db';
+
+const getEntries = async () => {
+  const user = await getUserFromClerkID();
+  const data = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      analysis: true,
+    },
+  });
+
+  return data;
+};
+
 const JournalPage = async () => {
+  const entries = await getEntries();
+  console.log('entries', entries);
   return <div>journal</div>;
 };
 
