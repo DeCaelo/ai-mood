04b8593d4b5f0c9ae40f25b7dc798508385bdb7b
DeCaelo <ludovic.cleuet@gmail.com>
2023-09-20 10:24:45 +0200

    link clerkid

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 - Local address to connect your application: 127.0.0.1:3309
 
 ```console
-npx prisma db push
-npx prisma format
+  bunx prisma db push
+  bunx prisma format
+  bunx prisma studio
 ```
--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,10 +1,36 @@
 import { Button } from '@/components/ui/button';
 import { Loader } from 'lucide-react';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { prisma } from '@/utils/db';
+
+const createNewUser = async () => {
+  const user = await currentUser();
+  console.log(user);
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  });
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    });
+  }
+
+  redirect('/journal');
+};
 
 const NewUser = async () => {
+  await createNewUser();
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <Button>
+      <Button disabled>
         <span className="mr-2 inline-flex shrink-0 self-center animate-spin-slow">
           <Loader className="w-4" />
         </span>
