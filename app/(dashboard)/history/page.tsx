cab035f38d3975089776a5b6c8190597dce01bec
DeCaelo <ludovic.cleuet@gmail.com>
2023-09-25 17:08:48 +0200

    no analyses

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -23,15 +23,24 @@ const getData = async () => {
 
 const HistoryPage = async () => {
   const { analyses, average } = await getData();
+
   return (
-    <div className="h-full px-6 py-8">
-      <h1 className="text-vertfluo text-2xl mb-4">
-        Avg. Sentiment: <strong>{average}</strong>
-      </h1>
-      <div className="h-full w-full">
-        <HistoryChart data={analyses} />
-      </div>
-    </div>
+    <>
+      {analyses.length > 0 ? (
+        <div className="h-full px-6 py-8">
+          <h1 className="text-vertfluo text-2xl mb-4">
+            Avg. Sentiment: <strong>{average}</strong>
+          </h1>
+          <div className="h-full w-full">
+            <HistoryChart data={analyses} />
+          </div>
+        </div>
+      ) : (
+        <div className="h-full px-6 py-8">
+          <h1 className="text-vertfluo text-2xl mb-4">Not yet analyses</h1>
+        </div>
+      )}
+    </>
   );
 };
 
