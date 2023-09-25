41ff9e91252e0979eaa999b48e420de2e4abc706
DeCaelo <ludovic.cleuet@gmail.com>
2023-09-25 11:44:54 +0200

    question form

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,10 +1,62 @@
 'use client';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const Question = () => {
+  const formSchema = z.object({
+    search: z.string().min(2, {
+      message: 'search must be at least 2 characters.',
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      search: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <div className="px-4 py-5 sm:p-6">
-      <span className="text-3xl">Question</span>
-    </div>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex items-center"
+      >
+        <FormField
+          control={form.control}
+          name="search"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="w-64"
+                  placeholder="Ask a question"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button variant={'secondary'} className="bg-green-400 ml-3">
+          Ask
+        </Button>
+      </form>
+    </Form>
   );
 };
 
